06e13df99701a5aaae68d98e85f0f49bc596203b
Andrew <andrew.weinmann@gmail.com>
2022-03-17 18:38:12 -0500

    Add support for bullet trails mod

--- data-updates.lua
+++ data-updates.lua
@@ -2,5 +2,5 @@ local Constants = require("constants")
 
 if mods["bullet-trails"] then
     require("__bullet-trails__/data-updates")
-    add_trail_to_ammo(Constants.magazine, "bullet-beam-orange")
+    add_trail_to_ammo(Constants.magazine, "bullet-beam-blue")
 end
