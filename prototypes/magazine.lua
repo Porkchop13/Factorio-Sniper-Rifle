23299486a928e528ca9fe49936532dec7c5b3989
Andrew <andrew.weinmann@gmail.com>
2022-03-16 21:57:55 -0500

    Create items from scratch

--- prototypes/magazine.lua
+++ prototypes/magazine.lua
@@ -3,21 +3,42 @@ local Recipe = require("__stdlib__/stdlib/data/recipe")
 
 -- Item --
 local container = {}
-item = table.deepcopy(data.raw["ammo"]["piercing-rounds-magazine"])
-item.name = Constants.magazine
-item.icon = "__" .. Constants.modName .. "__/graphics/" .. Constants.magazine ..
-                ".png"
-item.icon_size = 64
-item.ammo_type.category = Constants.category
-item.ammo_type.action.action_delivery.target_effects = {
-    {
-        type = "create-entity",
-        entity_name = "explosion-hit",
-        offsets = {{0, 1}},
-        offset_deviation = {{-0.5, -0.5}, {0.5, 0.5}}
-    }, {type = "damage", damage = {amount = 120, type = "physical"}}
+local item = {
+    type = "ammo",
+    name = Constants.magazine,
+    icon = "__" .. Constants.modName .. "__/graphics/" .. Constants.magazine ..
+        ".png",
+    icon_size = 64,
+    ammo_type = {
+        category = Constants.category,
+        action = {
+            type = "direct",
+            action_delivery = {
+                type = "instant",
+                source_effects = {
+                    type = "create-explosion",
+                    entity_name = "explosion-gunshot"
+                },
+                target_effects = {
+                    {
+                        type = "create-entity",
+                        entity_name = "explosion-hit",
+                        offsets = {{0, 1}},
+                        offset_deviation = {{-0.5, -0.5}, {0.5, 0.5}}
+                    },
+                    {
+                        type = "damage",
+                        damage = {amount = 120, type = "physical"}
+                    }
+                }
+            }
+        }
+    },
+    magazine_size = 5,
+    subgroup = "ammo",
+    order = "a[basic-clips]-d[" .. Constants.magazine .. "]",
+    stack_size = 200
 }
-item.order = "a[basic-clips]-d[" .. Constants.magazine .. "]"
 table.insert(container, item)
 data:extend(container)
 
--- prototypes/rifle.lua
+++ prototypes/rifle.lua
@@ -3,16 +3,36 @@ local Recipe = require("__stdlib__/stdlib/data/recipe")
 
 -- Item --
 local container = {}
-local item = table.deepcopy(data.raw["gun"]["submachine-gun"])
-item.name = Constants.rifle
-item.icon = "__" .. Constants.modName .. "__/graphics/" .. Constants.rifle ..
-                ".png"
-item.icon_size = 64
-item.attack_parameters.ammo_category = Constants.category
-item.attack_parameters.cooldown = 60
-item.attack_parameters.damage_modifer = 6
-item.attack_parameters.range = 60
-item.order = "a[basic-clips]-c[" .. Constants.rifle .. "]"
+local item = {
+    type = "gun",
+    name = Constants.rifle,
+    icon = "__" .. Constants.modName .. "__/graphics/" .. Constants.rifle ..
+        ".png",
+    icon_size = 64,
+    subgroup = "gun",
+    order = "a[basic-clips]-c[" .. Constants.rifle .. "]",
+    attack_parameters = {
+        type = "projectile",
+        ammo_category = Constants.category,
+        cooldown = 60,
+        damage_modifer = 6,
+        movement_slow_down_factor = 0.5,
+        shell_particle = {
+            name = "shell-particle",
+            direction_deviation = 0.1,
+            speed = 0.1,
+            speed_deviation = 0.03,
+            center = {0, 0.1},
+            creation_distance = -0.5,
+            starting_frame_speed = 0.4,
+            starting_frame_speed_deviation = 0.1
+        },
+        projectile_creation_distance = 1.125,
+        range = 60
+    },
+    stack_size = 1
+}
+
 table.insert(container, item)
 data:extend(container)
 
